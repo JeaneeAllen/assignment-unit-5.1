7aa8eb58c2045e4e89631875be86f9fcb04f4a5d
Jeanee Allen <allenjeanee@yahoo.com>
2024-07-02 01:58:42 -0500

    answered q4: added 1 to my shoecount. answered q5: added favColor to array

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -45,6 +45,8 @@ console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
   - Console.log your updated shoe count. 
 */
 
+me.shoeCount += 1;
+console.log('Updated shoe count:', me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -52,3 +54,6 @@ console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'Blue'; 
+console.log(me);
